93e855d7ab2192a74d997c9777a5f4940fa21e7a
franz <franzisca@disroot.org>
2020-05-21 17:13:25 -0300

    Cambio en script personal

--- run-dev.sh
+++ run-dev.sh
@@ -2,7 +2,7 @@
 pgrep dockerd > /dev/null || sudo systemctl start docker
 
 # si el container no está corriendo, arrancarlo
-docker inspect --format="{{.State.Status}}" mongodb-vl | grep -q running || docker start mongodb-vl
+docker inspect --format="{{.State.Status}}" mongodb-unr | grep -q running || docker start mongodb-unr
 
 # si no estamos usando node v6, cambiar a esa
 node -v | grep -q v6 || nvm use v6.17.1
