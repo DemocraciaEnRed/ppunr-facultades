f63c60654f4b2b55e55a852d6bfad74aaef26d16
franz <franzisca@disroot.org>
2020-06-03 12:44:43 -0300

    Fix bug en función cuando viene valor vacío

--- ext/lib/site/home-propuestas/topic-card/component.js
+++ ext/lib/site/home-propuestas/topic-card/component.js
@@ -50,6 +50,7 @@ export class TopicCard extends Component {
     )
 
     function capitalizeFirstLetter(str) {
+      if (!str) return ''
       return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
     }
 
