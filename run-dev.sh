7506769cf5982913833ce40ebb58c8cf1eaaa7a2
franz <franzisca@disroot.org>
2020-06-10 14:49:18 -0300

    Script adaptado a nuevo container de mongo

--- run-dev.sh
+++ run-dev.sh
@@ -2,7 +2,8 @@
 pgrep dockerd > /dev/null || sudo systemctl start docker
 
 # si el container no está corriendo, arrancarlo
-docker inspect --format="{{.State.Status}}" mongodb-unr | grep -q running || docker start mongodb-unr
+# crear el container con: docker run -dp 27018:27017 --name mongodb-unr-escuelas mongo:3.2 
+docker inspect --format="{{.State.Status}}" mongodb-unr-escuelas | grep -q running || docker start mongodb-unr-escuelas
 
 # si no estamos usando node v6, cambiar a esa
 node -v | grep -q v6 || nvm use v6.17.1
