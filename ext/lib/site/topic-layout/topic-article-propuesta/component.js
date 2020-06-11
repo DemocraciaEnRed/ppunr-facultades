5c61c6b19df0d5920ba8e2405baa0c3cf961161f
franz <franzisca@disroot.org>
2020-06-11 13:11:49 -0300

    Arreglar click en editar idea

--- ext/lib/site/topic-layout/topic-article-propuesta/component.js
+++ ext/lib/site/topic-layout/topic-article-propuesta/component.js
@@ -148,7 +148,7 @@ class TopicArticle extends Component {
           (forum.privileges && forum.privileges.canChangeTopics)
             ? (
               <div className='topic-article-content topic-admin-actions'>
-                <Link href={`/formulario-propuesta/${topic.id}`}>
+                <Link href={`/formulario-propuesta/${topic.id}?id=${topic.escuela._id}`}>
                   <a className='btn btn-default'>
                     <i className='icon-pencil' />
                     &nbsp;
@@ -162,7 +162,7 @@ class TopicArticle extends Component {
                (
                  <div className='topic-article-content topic-admin-actions'>
                    <a
-                     href={`/formulario-propuesta/${topic.id}`}
+                     href={`/formulario-propuesta/${topic.id}?id=${topic.escuela._id}`}
                      className='btn btn-default'>
                      <i className='icon-pencil' />
                       &nbsp;
