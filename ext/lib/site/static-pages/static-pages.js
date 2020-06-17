61bebd7bb5b3f3d62b55339be83a88e12de13e81
franz <franzisca@disroot.org>
2020-06-17 12:09:14 -0300

    Sacar static pages (datos/documentos/reglamento/herramientas) innecesarios

--- ext/lib/site/static-pages/static-pages.js
+++ ext/lib/site/static-pages/static-pages.js
@@ -1,11 +1,7 @@
 import 'lib/boot/routes'
 import router from 'lib/site/boot/router'
 import AcercaDe from './pages/acerca-de'
-import Datos from './pages/datos'
 import TyC from './pages/terminos-y-condiciones'
-//import Reglamento from './pages/reglamento'
-import Herramientas from './pages/herramientas'
-import Documentos from './pages/documentos'
 
 router.childRoutes.unshift({
   path: 's/acerca-de',
@@ -13,26 +9,6 @@ router.childRoutes.unshift({
 })
 
 router.childRoutes.unshift({
-  path: 's/datos',
-  component: Datos
-})
-
-router.childRoutes.unshift({
   path: 's/terminos-y-condiciones',
   component: TyC
-})
-
-/*router.childRoutes.unshift({
-  path: 's/reglamento',
-  component: Reglamento
-})*/
-
-router.childRoutes.unshift({
-  path: 's/herramientas',
-  component: Herramientas
-})
-
-router.childRoutes.unshift({
-  path: 's/documentos',
-  component: Documentos
 })
