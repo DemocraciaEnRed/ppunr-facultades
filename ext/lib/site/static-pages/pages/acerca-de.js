0312efb1e7597f29d011c11db88927dcfd970d51
Sabrina del Poy <sabriidelpoy@gmail.com>
2020-06-20 13:34:49 -0300

    textos acerca-de

--- ext/lib/site/static-pages/pages/acerca-de.js
+++ ext/lib/site/static-pages/pages/acerca-de.js
@@ -35,12 +35,12 @@ export default class Page extends Component {
                 <p className='p-padding'>Podés leer el reglamento completo haciendo click <a href="https://presupuestoparticipativo.unr.edu.ar/?page_id=1551" rel="noopener noreferer" target="_blank">aquí</a></p>
 
                 <Accordion>
-                  <div data-trigger="+ ¿Quiénes pueden participar del PP Facultades?">
-                    <p className='p-padding'>Pueden participar docentes, nodocentes, estudiantes, graduados y graduadas de las 12 Facultades y las Sedes de la UNR.</p>
+                  <div data-trigger="+ ¿Quiénes pueden participar del PP Escuelas?">
+                    <p className='p-padding'>Pueden participar docentes, nodocentes, estudiantes, graduados y graduadas de cada Escuela: Agrotécnica, Superior, Politécnico.</p>
                   </div>
 
-                  <div data-trigger="+ ¿En qué se basa la etapa de Foro del PPUNR?" data-triggerDisabled={true}>
-                    <p className='p-padding'>El foro tiene por objetivo que las/os integrantes de la comunidad propongan ideas que aporten en la construcción de la Universidad que queremos. Estará abierto desde el 29 de junio al 20 de julio. En la segunda etapa del Foro, a partir del 3 de agosto, se podrán inscribir quienes  deseen ser proyectistas y trabajar para convertir las ideas en proyectos.</p>
+                  <div data-trigger="+ ¿En qué se basa la etapa de Foros del PP Escuelas?" data-triggerDisabled={true}>
+                    <p className='p-padding'>Cada Escuela tendrá su propio Foro. El foro tiene por objetivo que las/os integrantes de la comunidad propongan ideas que aporten en la construcción de la Escuela que quieren hacer realidad. Estará abierto desde el 29 de junio al 20 de julio. En la segunda etapa del Foro, a partir del 3 de agosto, se podrán inscribir quienes  deseen ser proyectistas y trabajar para convertir las ideas en proyectos.</p>
                   </div>
 
                   <div data-trigger="+ ¿Cómo participo?">
@@ -57,20 +57,25 @@ export default class Page extends Component {
                   </div>
 
                   <div data-trigger="+ ¿Qué pasa si no puedo registrarme?">
-                    <p className='p-padding'>Te invitamos a que nos mandes un correo con todos tus datos (nombre completo, DNI, Facultad, claustro, mail o forma de contacto) a <a href="mailto:presupuestoparticipativo@unr.edu.ar">presupuestoparticipativo@unr.edu.ar</a></p>
+                    <p className='p-padding'>Te invitamos a que nos mandes un correo con todos tus datos (nombre completo, DNI, Facultad, claustro, mail o forma de contacto) a al mail de tu escuela:
+                      <ul>
+                        <li>Agrotécnica: <a href="mailto:presupuestoparticipativoagro@gmail.com">presupuestoparticipativoagro@gmail.com</a></li>
+                        <li>Superior: <a href="mailto:superiorparticipativo@unr.edu.ar">superiorparticipativo@unr.edu.ar</a></li>
+                        <li>Politécnico: <a href="mailto:presupuestoparticipativo@ips.edu.ar">presupuestoparticipativo@ips.edu.ar</a></li>
+                      </ul>
+                    </p>
                   </div>
 
                   <div data-trigger="+ ¿Cuántas ideas puedo subir?">
-                    <p className='p-padding'>Podés subir todas las ideas que quieras pero para cargar cada una de ellas tenés que utilizar un formulario independiente.</p>
+                    <p className='p-padding'>Podés subir todas las ideas que quieras, pero para cargar cada una de ellas tenés que utilizar un formulario independiente.</p>
                   </div>
 
-                  <div data-trigger="+ ¿Qué tipo de ideas esperamos que subas al Foro del PP Facultades?">
-                    <p className='p-padding'>Las ideas tienen que implicar aportes para más de una Facultad o para los espacios comunes (como el gimnasio o los comedores). No pueden ser ideas que sólo impacten en una Facultad. Esto tiene por objetivo que pensemos a la Universidad en un sentido más transversal e inclusivo, así como generar propuestas multidisciplinarias.</p>
-                    <p>En el caso del PP Escuelas, las personas que integran el Politécnico, el Superior y la Agrotécnica propondrán ideas que impacten sobre su propia comunidad educativa.</p>
+                  <div data-trigger="+ ¿Qué tipo de ideas esperamos que subas al Foro?">
+                    <p className='p-padding'>Las ideas tienen que beneficien a la comunidad de tu escuela.</p>
                   </div>
 
                   <div data-trigger="+ ¿Qué pasará con mi idea?">
-                    <p className='p-padding'>Otros/as participantes pueden comentar tu idea o apoyarla. Te invitamos a entrar en diálogo con otros/as participantes. Luego de la primera etapa del Foro (del 29 de Junio al 20 de Julio) haremos una sistematización de ideas por temas para colocarlas de manera ordenada en la plataforma para que, si te interesa sumarte como proyectista, te inscribas en la que quieras desarrollar como proyecto en el marco de la Comisión Universitaria.</p>
+                    <p className='p-padding'>Otros/as participantes pueden comentar tu idea o apoyarla. Te invitamos a entrar en diálogo con otros/as participantes. Luego de la primera etapa del Foro (del 29 de Junio al 20 de Julio) haremos una sistematización de ideas por temas para colocarlas de manera ordenada en la plataforma para que, si te interesa sumarte como proyectista, te inscribas en la que quieras desarrollar como proyecto en el marco del Consejo Escolar.</p>
                   </div>
 
                   <div data-trigger="+ ¿Cuáles son las dos etapas de los Foros?">
@@ -84,42 +89,25 @@ export default class Page extends Component {
                   </div>
 
                   <div data-trigger="+ ¿Qué implica ser proyectista?">
-                    <p className='p-padding'>Luego de los Foros, la segunda etapa del PP es la conformación de la Comisión Universitaria. La misma estará integrada por todas las personas que se hayan propuesto para transformar las ideas en proyectos. Funcionará durantes dos meses (agosto- octubre) en el marco de encuentros con técnicos de la Universidad que contribuirán a darle factibilidad a los proyectos que serán elegidos por la comunidad y serán ejecutados en 2021.</p>
+                    <p className='p-padding'>Luego de los Foros, la segunda etapa del PP es la conformación del Consejo Escolar. El mismo estará integrada por todas las personas que se hayan propuesto para transformar las ideas en proyectos. Funcionará durantes dos meses (agosto- octubre) en el marco de encuentros con técnicos de la Universidad que contribuirán a darle factibilidad a los proyectos que serán elegidos por la comunidad y se ejecutarán en 2021.</p>
                   </div>
 
                   <div data-trigger="+ ¿Qué condiciones deben respetar los proyectos?">
                     <ul className='p-padding'>
                       <li>Ser elaborados por integrantes de más de un claustro.</li>
-                      <li>Incluir resultados para más de una unidad académica o algún espacio común</li>
-                      <li>No exceder el límite presupuestario.</li>
+                      <li>No exceder el límite presupuestario (1 millón de pesos)</li>
                       <li>No exceder el ámbito de la Universidad.</li>
                       <li>No afectar partidas presupuestarias de años posteriores.</li>
                       <li>Ser factibles técnicamente para poder ser ejecutados en caso de ser elegido.</li>
                     </ul>
                   </div>
 
-                  <div data-trigger="+ ¿Qué espacios podemos intervenir con proyectos del PP?">
-                    <p className='p-padding'>Los proyectos deben ser desarrollados por integrantes de por lo menos dos claustros. Tiene que incluir resultados para más de una unidad académica o bien para algún espacio común de la Universidad (comedores, gimnasios, Sede, Rectorado, CUR, anexo, etc)</p>
-                  </div>
-
-                  <div data-trigger="+ ¿Cuál es el monto asignado al PPUNR edición 2020?">
-                    <p className='p-padding'>Dieciocho millones de pesos, de los cuáles, cada Escuela tendrá disponible un millón para discutir en el marco de su comunidad, y el resto, quince millones, estará destinado al PP Facultades.</p>
+                  <div data-trigger="+ ¿Cuál es el monto asignado para cada Escuela en el PPUNR 2020?">
+                    <p className='p-padding'>Cada Escuela tendrá disponible un millón para discutir en el marco de su comunidad.</p>
                   </div>
 
                   <div data-trigger="+ ¿Cómo elegiremos los proyectos a ejecutarse en 2021?">
-                    <p className='p-padding'>Se realizarán jornadas de votación en el mes de octubre/noviembre, previa difusión de los proyectos elegibles, para que toda la comunidad de la UNR puede decidir cuáles serán ejecutarán hasta alcanzar el total de la partida presupuestaria afectada al PP Facultades. Es decir que, de acuerdo al monto de los proyectos elegidos, podrán ejecutarse uno o más proyectos. </p>
-                  </div>
-
-                  <div data-trigger="+ ¿Pueden participar los/las estudiantes de posgrado?">
-                    <p className='p-padding'>Sí, pueden participar todos/as los/las estudiantes de posgrado. </p>
-                  </div>
-
-                  <div data-trigger="+ ¿Pueden participar los/las estudiantes y docentes de ProUaPam (Programa para Adultos Mayores)?">
-                    <p className='p-padding'>Sí, pueden participar todos/as los/las integrantes del PRoUaPam.</p>
-                  </div>
-
-                  <div data-trigger="+ ¿ Pueden participar los/las integrantes de  las Extensiones áulicas?">
-                    <p className='p-padding'>Sí, pueden participar todos/as los/las integrantes de las extensiones áulicas.</p>
+                    <p className='p-padding'>Se realizarán jornadas de votación en el mes de octubre/noviembre, previa difusión de los proyectos elegibles, para que toda la comunidad de la Escuela puede decidir cuáles serán ejecutarán hasta alcanzar el total de la partida presupuestaria disponible. Es decir que, de acuerdo al monto de los proyectos elegidos, podrán ejecutarse uno o más proyectos.</p>
                   </div>
 
                 </Accordion>
