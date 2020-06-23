a9a87316c163bf1b73c90acc97a3c1aaf9363a38
franz <franzisca@disroot.org>
2020-06-23 12:50:25 -0300

    Fix menú de navegación para celus

--- ext/lib/site/header/mobile-menu/component.js
+++ ext/lib/site/header/mobile-menu/component.js
@@ -43,7 +43,7 @@ class MobileMenu extends Component {
                     onClick={this.props.toggleOnClick}>
                     Acerca de
                   </Link>
-                </div> 
+                </div>
                 {/* <div className='header-item'>
                   <Link
                     to='/propuestas'
@@ -57,11 +57,11 @@ class MobileMenu extends Component {
                   <div
                     key={escuela._id}
                     className={`header-item mobile-link ${window.location.href.includes(`propuestas?id=${escuela._id}`) ? 'active' : ''}`}>
-                    <Link
-                      to={`/propuestas?id=${escuela._id}`}
+                    <a
+                      href={`/propuestas?id=${escuela._id}`}
                       className={`header-link header-link-${escuela.abreviacion}`}
                       onClick={this.props.toggleOnClick}> Foro {escuela.nombre} ({escuela.abreviacion})
-                    </Link>
+                    </a>
                   </div>
                 ))}
                 {/* <div className='header-item mobile-link'>
