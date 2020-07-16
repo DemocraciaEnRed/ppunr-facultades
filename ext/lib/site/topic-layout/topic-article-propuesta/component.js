0b3b2585a949dca73c0d5141a9632c293142f4f8
franz <franzisca@disroot.org>
2020-07-16 12:15:03 -0300

    Comentario de moderador + link a idea integrada en formulario admin

--- ext/lib/site/topic-layout/topic-article-propuesta/component.js
+++ ext/lib/site/topic-layout/topic-article-propuesta/component.js
@@ -208,10 +208,10 @@ class TopicArticle extends Component {
               <div className='alert alert-info alert-propuesta' role='alert'>
                 <p>{topic.attrs['admin-comment']}</p>
 
-                {topic.attrs.state === 'integrado' && topic.attrs['admin-comment-referencia'] && topic.attrs['admin-comment-referencia'] !== '' &&
-                  <p className='admin-comment-referido'>Podés ver el proyecto final <a className='admin-comment-referido' href={topic.attrs['admin-comment-referencia']}>aquí</a>.</p>
+                {topic.attrs['admin-comment-referencia'] && topic.attrs['admin-comment-referencia'] !== '' &&
+                  <p className='admin-comment-referido'>Podés ver la idea final <a className='admin-comment-referido' href={topic.attrs['admin-comment-referencia']}>aquí</a>.</p>
                 }
-                <p className='font-weight-bold'>Universidad de Rosario</p>
+                <p className='font-weight-bold'>Presupuesto Participativo de la Universidad de Rosario</p>
               </div>
             )
         }
