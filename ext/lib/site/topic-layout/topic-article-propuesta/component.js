c7898f0ff37c55be7ffdc188831600bab12cfff6
franz <franzisca@disroot.org>
2020-07-30 17:07:49 -0300

    Cambios de texto en mail y texto de comentario admin

--- ext/lib/site/topic-layout/topic-article-propuesta/component.js
+++ ext/lib/site/topic-layout/topic-article-propuesta/component.js
@@ -216,7 +216,7 @@ class TopicArticle extends Component {
                 <p>{topic.attrs['admin-comment']}</p>
 
                 {topic.attrs['admin-comment-referencia'] && topic.attrs['admin-comment-referencia'] !== '' &&
-                  <p className='admin-comment-referido'>Podés ver la idea integrada <a className='admin-comment-referido' href={topic.attrs['admin-comment-referencia']}>aquí</a>.</p>
+                  <p className='admin-comment-referido'>Podés ver las ideas sistematizadas <a className='admin-comment-referido' href={topic.attrs['admin-comment-referencia']}>aquí</a>.</p>
                 }
                 <p className='font-weight-bold'>Equipo de Coordinación y Gestión PPUNR</p>
               </div>
