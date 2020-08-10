3a435f6ef2106fe3f5e49371a27751c9760a3ffc
franz <franzisca@disroot.org>
2020-08-10 16:47:13 -0300

    Nuevo script de mongo de exportación de datos de usuarios a csv

--- mongo-scripts/dump-users-facultades.js
+++ mongo-scripts/dump-users-facultades.js
@@ -1,23 +1,8 @@
 /* Ejecutar haciendo túnel con servidor mongo a localhost:26017 y después:
-mongo --quiet localhost:26017/ppunr-prod dump-users.js > ppunr-facultades-usuarios.csv
-mongo --quiet localhost:26017/ppunre-prod dump-users.js > ppunr-escuelas-usuarios.csv
+mongo --quiet localhost:26017/ppunr-prod dump-users-facultades.js > ppunr-facultades-usuarios.csv
 */
 
 print('Nombre,Apellido,Email,Facultad,Claustro,Fecha de registro,Validado')
-
-// este sirve para escuelas:
-/*db.users.find().sort({createdAt: -1}).forEach(u => {
-	let d = u.createdAt 
-	// convertimos a GMT-3
-	d.setHours(d.getHours() - 3)
-	print(
-		`${u.firstName},` +
-		`${u.lastName},` +
-		`${u.email},` +
-		`${d.toISOString()},` + 
-		`${u.emailValidated?'S':'N'}`
-	)
-})*/
 
 db.users.aggregate([
   //{$match: { claustro: { $ne: null } } },
