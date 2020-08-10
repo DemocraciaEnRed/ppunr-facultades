7197a1a1b35ad784b468b0e002a6367d2f260945
franz <franzisca@disroot.org>
2020-08-10 11:43:08 -0300

    Nuevo script de exportación de usuarios de mongo (con join/aggregate)

--- mongo-scripts/dump-users.js
+++ mongo-scripts/dump-users.js
@@ -3,9 +3,10 @@ mongo --quiet localhost:26017/ppunr-prod dump-users.js > ppunr-facultades-usuari
 mongo --quiet localhost:26017/ppunre-prod dump-users.js > ppunr-escuelas-usuarios.csv
 */
 
-print('Nombre,Apellido,Email,Fecha de registro,Validado')
+print('Nombre,Apellido,Email,Facultad,Claustro,Fecha de registro,Validado')
 
-db.users.find().sort({createdAt: -1}).forEach(u => {
+// este sirve para escuelas:
+/*db.users.find().sort({createdAt: -1}).forEach(u => {
 	let d = u.createdAt 
 	// convertimos a GMT-3
 	d.setHours(d.getHours() - 3)
@@ -13,6 +14,39 @@ db.users.find().sort({createdAt: -1}).forEach(u => {
 		`${u.firstName},` +
 		`${u.lastName},` +
 		`${u.email},` +
+		`${d.toISOString()},` + 
+		`${u.emailValidated?'S':'N'}`
+	)
+})*/
+
+db.users.aggregate([
+  //{$match: { claustro: { $ne: null } } },
+  // hacemos joins
+  {$lookup: {
+    from:'claustros',
+    localField:'claustro',
+    foreignField:'_id',
+    as:'claustro'
+  }},
+  {$lookup: {
+    from:'facultades',
+    localField:'facultad',
+    foreignField:'_id',
+    as:'facultad'
+  }},
+  {$unwind: {path:'$claustro'} },
+  {$unwind: {path:'$facultad'} },
+  {$sort: {createdAt: -1}}
+]).forEach(u => {
+	let d = u.createdAt 
+	// convertimos a GMT-3
+	d.setHours(d.getHours() - 3)
+	print(
+		`${u.firstName},` +
+		`${u.lastName},` +
+		`${u.email},` +
+		`${u.facultad.abreviacion},` +
+		`${u.claustro.nombre},` +
 		`${d.toISOString()},` + 
 		`${u.emailValidated?'S':'N'}`
 	)
