2811703133359abd8129e3f548890f6daaada599
franz <franzisca@disroot.org>
2020-11-30 16:52:21 -0300

    Ocultar preguntas vinculadas a los Foros en el acerca de

--- ext/lib/site/static-pages/pages/acerca-de.js
+++ ext/lib/site/static-pages/pages/acerca-de.js
@@ -39,58 +39,12 @@ export default class Page extends Component {
                     <p className='p-padding'>Pueden participar docentes, nodocentes, estudiantes, graduados y graduadas de las 12 Facultades y otras Sedes y dependencias de la UNR.</p>
                   </div>
 
-                  <div data-trigger="+ ¿En qué se basa la etapa de Foro del PPUNR?" data-triggerDisabled={true}>
-                    <p className='p-padding'>El Foro tiene por objetivo que las/os integrantes de la comunidad propongan ideas que aporten en la construcción de la Universidad que queremos. Estará abierto desde el 29 de junio al 20 de julio. En la segunda etapa del Foro, a partir del 3 de agosto, se podrán inscribir quienes  deseen ser proyectistas y trabajar para convertir las ideas en proyectos.</p>
-                  </div>
-
                   <div data-trigger="+ ¿Cómo participo?">
                     <p className='p-padding'>Te invitamos a registrarte <Link to='/signup'>aquí</Link> para luego ingresar en UNR Decide y poder votar. Podrás elegir un proyecto que de resultar ganador se ejecutará durante 2021.</p>
                   </div>
 
-                  <div data-trigger="+ ¿Cuáles son los temas para proponer ideas?">
-                  <p className='p-padding'>Vas a encontrar en el <Link to='/formulario-idea'>formulario</Link> una serie de etiquetas que refieren a diversos temas:</p>
-                  <p>Accesibilidad - Administración - Ambiente y Sustentabilidad - Aprendizajes y actividades académicas - Arte y Cultura - Bioseguridad Convivencia y Participación - Derechos Humanos - Género - Inclusión Infraestructura - Innovación - Internacionalización - Investigación - Recreación y deporte  - Salud - Tecnología - Transparencia Vinculación con el medio</p>
-                  </div>
-
-                  <div data-trigger="+ ¿Cómo subo una idea?">
-                    <p className='p-padding'>Encontrarás <Link to='/formulario-idea'>aquí</Link> el espacio para colocar tu idea: incluí un título y escribí un breve párrafo explicándola. Agregá etiquetas preconfiguradas para clasificar tu idea. Eso ayudará a agruparlas por afinidad para la próxima fase del Foro. </p>
-                  </div>
-
                   <div data-trigger="+ ¿Qué pasa si no puedo registrarme?">
                     <p className='p-padding'>Te invitamos a que nos mandes un correo con todos tus datos (nombre completo, DNI, Facultad, claustro, mail o forma de contacto) a <a href="mailto:presupuestoparticipativo@unr.edu.ar">presupuestoparticipativo@unr.edu.ar</a></p>
-                  </div>
-
-                  <div data-trigger="+ ¿Cuántas ideas puedo subir?">
-                    <p className='p-padding'>Podés subir todas las ideas que quieras aunque cada una en un formulario independiente.</p>
-                  </div>
-
-                  <div data-trigger="+ ¿Qué tipo de ideas esperamos que subas al Foro del PP Facultades?">
-                    <p className='p-padding'>Las ideas tienen que implicar aportes para más de una Facultad o para los espacios comunes (como el gimnasio o los comedores). No pueden ser ideas que sólo impacten en una Facultad. Esto tiene por objetivo que pensemos a la Universidad en un sentido más transversal e inclusivo, así como generar propuestas multidisciplinarias.</p>
-                  </div>
-
-                  <div data-trigger="+ ¿Puedo modificar mi idea una vez que fue enviada?">
-                    <p className='p-padding'>Si, puedes modificar tu idea tantas veces como quieras mientras el Foro esté abierto.</p>
-                  </div>
-
-                  <div data-trigger="+ ¿Cuándo se cierra el Foro?">
-                    <p className='p-padding'>El Foro se cerrará, en una primera fase, el 20 de Julio. Hasta entonces tenes tiempo para modificar tu idea e intercambiar sobre las ideas de otros/as participantes. Luego, se abrirá el 3 de agosto y se dará por finalizada la etapa el 14 del mismo mes, fecha en la cual podes revisar las ideas y sumarte para transformarlas en proyectos.</p>
-                  </div>
-
-                  <div data-trigger="+ ¿Qué pasará con mi idea?">
-                    <p className='p-padding'>Otros/as participantes pueden comentar tu idea o apoyarla. Te invitamos a entrar en diálogo con otros/as participantes. Luego de la primera etapa del Foro (del 29 de Junio al 20 de Julio) haremos una sistematización de ideas por temas para colocarlas de manera ordenada en la plataforma para que, si te interesa sumarte como proyectista, te inscribas en la que quieras desarrollar como proyecto en el marco de la Comisión Universitaria.</p>
-                  </div>
-
-                  <div data-trigger="+ ¿Cuáles son las dos etapas de los Foros?">
-                    <p className='p-padding'>La primera es del 29 de junio al 20 de julio para que subas tu idea y/o comentes las de otros/as.</p>
-                    <p>La segunda del 3 de agosto al 14 de agosto será para que te propongas como proyectista de las ideas que desees desarrollar para que se transforme en un proyecto factible.</p>
-                  </div>
-
-                  <div data-trigger="+ Si no participé de una etapa anterior, ¿puedo sumarme?">
-                    <p className='p-padding'>Podés sumarte en cualquier etapa del proceso aunque no hayas participado de las anteriores. Es decir, podés no haber propuesto o comentado ideas pero interesarte por sumar tu aporte como proyectista.</p>
-                  </div>
-
-                  <div data-trigger="+ ¿Qué implica ser proyectista?">
-                    <p className='p-padding'>Luego de los Foros, la segunda etapa del PP es la conformación de la Comisión Universitaria. La misma estará integrada por todas las personas que se hayan propuesto para transformar las ideas en proyectos. Funcionará durante dos meses (agosto- octubre) en el marco de encuentros con técnicos de la Universidad que contribuirán a darle factibilidad a los proyectos que serán elegidos por la comunidad y serán ejecutados en 2021.</p>
                   </div>
 
                   <div data-trigger="+ ¿Qué condiciones deben respetar los proyectos?">
