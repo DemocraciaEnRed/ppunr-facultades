3b2e99bed0ec1732043851058c5464ad8deffc20
Guillermo Croppi <guillermocroppi@gmail.com>
2021-06-11 13:56:11 -0300

    Skipping 1503424667703-add-blacklist-email migration

--- migrations/1503424667703-add-blacklist-email.js
+++ migrations/1503424667703-add-blacklist-email.js
@@ -14,25 +14,25 @@ const mapPromises = (fn) => (array) => Promise.all(array.map(fn))
 
 exports.up = function up (done) {
   dbReady()
-    .then(() => config.blackListEmails ? Promise.resolve() : Promise.reject())
-    .then(() => User.collection.find({ email: { $in: config.blackListEmails.map((d) => new RegExp(`@${d}*$`)) } }).toArray())
-    .then((users) => User.collection.deleteMany({ email: { $in: config.blackListEmails.map((d) => new RegExp(`@${d}*$`)) } })
-      .then(() => Vote.collection.deleteMany({ author: { $in: users.map((u) => u._id) } })))
-    .then(() => Topic.collection.find({}).toArray())
-    .then(mapPromises((topic) => {
-      return calcResult(topic)
-      .then(({ count, results }) => {
-        const closed = topic.closingAt && topic.closingAt.getTime() < Date.now()
-        const action = {
-          count: closed ? topic.action.count : count,
-          results: closed ? topic.action.results : results,
-          method: topic.action.method
-        }
-        return Topic.collection.findOneAndUpdate({ _id: topic._id }, { $set: { 'action': action } })
-      })
-    }))
+    // .then(() => config.blackListEmails ? Promise.resolve() : Promise.reject())
+    // .then(() => User.collection.find({ email: { $in: config.blackListEmails.map((d) => new RegExp(`@${d}*$`)) } }).toArray())
+    // .then((users) => User.collection.deleteMany({ email: { $in: config.blackListEmails.map((d) => new RegExp(`@${d}*$`)) } })
+    //   .then(() => Vote.collection.deleteMany({ author: { $in: users.map((u) => u._id) } })))
+    // .then(() => Topic.collection.find({}).toArray())
+    // .then(mapPromises((topic) => {
+    //   return calcResult(topic)
+    //   .then(({ count, results }) => {
+    //     const closed = topic.closingAt && topic.closingAt.getTime() < Date.now()
+    //     const action = {
+    //       count: closed ? topic.action.count : count,
+    //       results: closed ? topic.action.results : results,
+    //       method: topic.action.method
+    //     }
+    //     return Topic.collection.findOneAndUpdate({ _id: topic._id }, { $set: { 'action': action } })
+    //   })
+    // }))
     .then(function () {
-      console.log(`Remove black listed emails.`)
+      console.log(`(Skipped) Remove black listed emails.`)
       done()
     })
     .catch(function (err) {
