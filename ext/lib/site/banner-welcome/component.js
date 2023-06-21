8d5e36646e21f92335e2031275f52c38d3937ace
xtian7489 <xtian7489@gmail.com>
2023-06-21 10:38:25 -0300

    fix banner welcome

--- ext/lib/site/banner-welcome/component.js
+++ ext/lib/site/banner-welcome/component.js
@@ -17,11 +17,11 @@ class BannerWelcome extends Component {
       <div id='bannerwelcome' className='banner-welcome'>
         <img src="/ext/lib/site/banner-welcome/vector.png" className="image-large" alt="Icono proyectistas"/>
         <p className="title"><strong>{
-          config.votacionAbierta ? '¡Ya iniciamos la votación!' : 
+          forumConfig.votacion ? '¡Ya iniciamos la votación!' : 
           `PPUNR ${config.currentEdition}` 
           }</strong></p>
         <p className="subtitle">{
-        config.votacionAbierta ? 'Podés votar hasta 3 proyectos' : 
+        forumConfig.votacion ? 'Podés votar hasta 3 proyectos' : 
         'Te invitamos a ver más información en: presupuestoparticipativo.unr.edu.ar'
         }</p>
 
